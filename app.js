01bbf0fefe12875ab5822f50dcb2e285ab435750
Sean Thomas Anderson <Anderson.Sean27@gmail.com>
2016-06-14 14:54:26 -0700

    first question finished

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 'use strict';
-alert('Hello!');
+var userName = prompt('Greetings! What is your name?');
+alert('Welcome, ' + userName + ', glad you\'re here!');
+
+var answer1 = prompt('Has Sean been to Bosnia?');
+if (answer1.toLowerCase() === 'yes' || answer1.toLowerCase() ===  'y') {
+  alert('Right! Sean studied abroad in Sarajevo.');
+} else {
+  alert('Wrong! Sean studied abroad in Sarajevo.');
+}

c34da3eb76c92cd8b81785c4cec02504607c85a8
Sean Thomas Anderson <Anderson.Sean27@gmail.com>
2016-06-14 15:33:09 -0700

    first css draft

--- app.js
+++ app.js
@@ -3,8 +3,15 @@ var userName = prompt('Greetings! What is your name?');
 alert('Welcome, ' + userName + ', glad you\'re here!');
 
 var answer1 = prompt('Has Sean been to Bosnia?');
-if (answer1.toLowerCase() === 'yes' || answer1.toLowerCase() ===  'y') {
+if (answer1.toLowerCase() === 'yes' || answer1.toLowerCase() === 'y') {
   alert('Right! Sean studied abroad in Sarajevo.');
 } else {
   alert('Wrong! Sean studied abroad in Sarajevo.');
+}
+
+var answer2 = prompt('Has Sean Written 4 novels?');
+if (answer2.toLowerCase() === 'no' || answer2.toLowerCase()==='n'){
+  alert('Right! Sean has written 3 novels.');
+} else {
+  alert('Wrong! Sean has written 3 novels.');
 }

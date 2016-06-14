a19c994e37a1ef438d86b446e97c63ce205c9e20
Sean Thomas Anderson <Anderson.Sean27@gmail.com>
2016-06-14 16:19:31 -0700

    app.js adjustments

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var userName = prompt('Greetings! What is your name?');
 alert('Welcome, ' + userName + ', glad you\'re here!');
 
 var answer1 = prompt('Has Sean been to Bosnia?');
+console.log('The user answered ' + answer1);
 if (answer1.toLowerCase() === 'yes' || answer1.toLowerCase() === 'y') {
   alert('Right! Sean studied abroad in Sarajevo.');
 } else {
@@ -10,13 +11,15 @@ if (answer1.toLowerCase() === 'yes' || answer1.toLowerCase() === 'y') {
 }
 
 var answer2 = prompt('Has Sean Written 4 novels?');
-if (answer2.toLowerCase() === 'no' || answer2.toLowerCase()==='n'){
+console.log('The user answered ' + answer2);
+if (answer2.toLowerCase() === 'no' || answer2.toLowerCase() === 'n'){
   alert('Right! Sean has written 3 novels.');
 } else {
   alert('Wrong! Sean has written 3 novels.');
 }
 
 var answer3 = prompt('Has Sean ran a marathon?');
+console.log('The user answered ' + answer3);
 if (answer3.toLowerCase() === 'yes' || answer3.toLowerCase() === 'y') {
   alert('Right! Sean ran the Last Chance Marathon in Bellingham.');
 } else {
@@ -24,6 +27,7 @@ if (answer3.toLowerCase() === 'yes' || answer3.toLowerCase() === 'y') {
 }
 
 var answer4 = prompt('Can Sean play the piano?');
+console.log('The user answered ' + answer4);
 if (answer4.toLowerCase() === 'no' || answer4.toLowerCase() === 'n') {
   alert('Right! Sean plays the drums.');
 } else {
@@ -36,7 +40,6 @@ if (answer5.toLowerCase() === 'yes' || answer5.toLowerCase() === 'y') {
   console.log('The user answered ' + answer5);
   alert('Right! Sean is a big fan of Radiohead.');
 } else {
-  console.log('You\'re in the negative zone.');
   alert('Wrong! Sean is a big fan of Radiohead.');
 }
-  console.log('You finished the quiz! :o)');
+console.log('You finished the quiz! :o)');

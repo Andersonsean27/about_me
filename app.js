5c8295ab0661270b3e52a9ddd73d4eb3fa634f28
Sean Thomas Anderson <Anderson.Sean27@gmail.com>
2016-06-14 16:12:04 -0700

    first complete draft

--- app.js
+++ app.js
@@ -15,3 +15,28 @@ if (answer2.toLowerCase() === 'no' || answer2.toLowerCase()==='n'){
 } else {
   alert('Wrong! Sean has written 3 novels.');
 }
+
+var answer3 = prompt('Has Sean ran a marathon?');
+if (answer3.toLowerCase() === 'yes' || answer3.toLowerCase() === 'y') {
+  alert('Right! Sean ran the Last Chance Marathon in Bellingham.');
+} else {
+  alert('Wrong! Sean ran the Last Chance Marathon in Bellingham.');
+}
+
+var answer4 = prompt('Can Sean play the piano?');
+if (answer4.toLowerCase() === 'no' || answer4.toLowerCase() === 'n') {
+  alert('Right! Sean plays the drums.');
+} else {
+  alert('Wrong! Sean plays the drums.');
+}
+
+var answer5 = prompt('Is Radiohead Sean\'s favorite band?');
+console.log('The user answered ' + answer5);
+if (answer5.toLowerCase() === 'yes' || answer5.toLowerCase() === 'y') {
+  console.log('The user answered ' + answer5);
+  alert('Right! Sean is a big fan of Radiohead.');
+} else {
+  console.log('You\'re in the negative zone.');
+  alert('Wrong! Sean is a big fan of Radiohead.');
+}
+  console.log('You finished the quiz! :o)');
